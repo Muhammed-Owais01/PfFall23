627705f1ecf90b3dbbfee9ff151fca969a0c2480
Muhammed-Owais01 <83649329+Muhammed-Owais01@users.noreply.github.com>
2023-10-10 10:58:59 +0500

    Add files via upload

---
+++ Labs/07/Q7.c
@@ -0,0 +1,21 @@
+/**
+ * Programmer: Muhammed Owais 
+ * Desc: 2x2 Matrix Multiplication
+ * Date: 10/10/2023
+ * Roll-No: 23K-0047
+ */ 
+#include <stdio.h>
+// NOT COMPLETED YET DONT CHECK THIS
+int main() {
+	int rows = 2;
+	int columns = 2;
+	int col2 = 0;
+	for(int i = 0; i < rows; i++) {
+		for(int j = 0; j < columns; j++) {
+			for(int a = 0; a < rows; a++) {
+				
+			}
+		}
+		col2++;
+	}
+}
