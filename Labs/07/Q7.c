ae205391fd2528aa0aea51e685d74247355db82f
Muhammed-Owais01 <83649329+Muhammed-Owais01@users.noreply.github.com>
2023-10-13 15:51:22 +0500

    Update Q7.c

--- Labs/07/Q7.c
+++ Labs/07/Q7.c
@@ -5,14 +5,30 @@
  * Roll-No: 23K-0047
  */ 
 #include <stdio.h>
-// NOT COMPLETED DONT CHECK
+
 int main() {
 	int rows = 2;
 	int columns = 2;
-	int arr1[20], arr2[20], newarray[20];
+	int sum = 0;
+	int arr1[20][20], arr2[20][20], newarray[20];
 	for(int i = 0; i < rows; i++) {
+		for(int j = 0; j < columns; j++) {
+		    printf("Enter Matrix 1 (%d,%d): ", i+1, j+1);
+		    scanf("%d", &arr1[i][j]);
+		}
+	}
+	for(int i = 0; i < rows; i++) {
+		for(int j = 0; j < columns; j++) {
+		    printf("Enter Matrix 2 (%d,%d): ", i+1, j+1);
+		    scanf("%d", &arr2[i][j]);
+		}
+	}
+	for(int i = 0; i < rows; i++) {
+	    int newcol = 0;
+	    while (newcol < column)
 		for(int j = 0; j < columns; j++) {
 				sum = sum + arr1[i][j] * arr2[j][i];
 		}
 	}
+	
 }
