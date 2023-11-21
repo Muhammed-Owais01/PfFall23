b5c0d50e9762400e88ef07f3b6b99ce296c81759
Muhammed-Owais01 <83649329+Muhammed-Owais01@users.noreply.github.com>
2023-11-21 09:47:09 +0500

    Add files via upload

---
+++ Labs/11/Q2.c
@@ -0,0 +1,42 @@
+/**
+ * Programmer: Muhammed Owais 
+ * Desc: Car Data
+ * Date: 21/11/2023
+ * Roll-No: 23K-0047
+ */ 
+#include <stdio.h>
+
+typedef struct car
+{
+    char id[5];
+    int year;
+    char material[50];
+    int quantity;
+} car;
+
+
+int main(){
+    car data[60];
+    int num;
+    printf("Enter Number of Serial Numbers: ");
+    scanf("%d", &num);
+    for (int i = 0; i < num; i++)
+    {
+        printf("Enter Serial Number of Car %d: ", i+1);
+        scanf("%s", &data[i].id);
+        printf("Enter Year of Car %d: ", i+1);
+        scanf("%d", &data[i].year);
+        printf("Enter Material of Car %d: ", i+1);
+        scanf("%s", &data[i].material);
+        printf("Enter Quantitiy of Car %d: ", i+1);
+        scanf("%d", &data[i].quantity);
+    }
+    for (int i = 0; i < num; i++)
+    {
+        if (strcmp(data[i].id, "BB1") > 0 && strcmp(data[i].id, "CC6") < 0)
+        {
+            printf("Serial Number: %s, Year: %d, Material: %s, Quantity: %d\n", data[i].id, data[i].year, data[i].material, data[i].quantity);
+        }
+    }
+    
+}
