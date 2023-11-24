4bfef3221799311c306463b2932eeb9b21803acb
Muhammed-Owais01 <83649329+Muhammed-Owais01@users.noreply.github.com>
2023-11-24 20:24:31 +0500

    Add files via upload

---
+++ Labs/10/Q4.c
@@ -0,0 +1,44 @@
+/**
+ * Programmer: Muhammed Owais 
+ * Desc: Reverse
+ * Date: 24/11/2023
+ * Roll-No: 23K-0047
+ */ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+	int length;
+	char temp;
+	printf("Enter The Length Of Your String: ");
+	scanf("%d", &length);
+	char *arr = (char *)malloc(length * sizeof(char));
+	printf("Enter A Sentence: ");
+	getchar();
+	fgets(arr, length + 1, stdin);
+	int i = 0, count = 0;
+	while(i <= length){
+		count++;
+		if (*(arr+i) == ' ' || *(arr + i) == '\0')
+		{
+			int range = i - (count / 2);
+			int last_letter = 0;
+			for (int j = i - (count - 1); j < range; ++j)
+			{
+				char temp = *(arr + j);
+				*(arr + j) = *(arr + i - 1 - last_letter);
+				*(arr + i - 1 - last_letter) = temp;
+				last_letter++;
+			}
+			count = 0;
+		}
+		if (*(arr + i) == ' ')
+		{
+			count = 0;
+		}
+		i++;
+	}
+	puts(arr);
+	free(arr);
+}
