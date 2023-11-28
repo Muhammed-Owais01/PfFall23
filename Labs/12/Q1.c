7d34f14eb8535ca823fec9ca1de63b714b8d137c
Muhammed-Owais01 <83649329+Muhammed-Owais01@users.noreply.github.com>
2023-11-28 10:22:19 +0500

    Add files via upload

---
+++ Labs/12/Q1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    FILE *ptr = fopen("CSV.csv", "r");
+
+    if (ptr == NULL)
+    {
+        printf("File DIDNT OPEN!\n");
+        return 0;
+    }
+
+    
+
+    char temp[50];
+    fgets(temp, 50, ptr);
+    int row = 0, col = 0;
+    char *token = strtok(temp, ",");
+    while (token != NULL)
+    {
+        token = strtok(NULL, ",");
+        col++;
+    }
+    rewind(ptr);
+    while (!(feof(ptr)))
+    {
+        fgets(temp, 50, ptr);
+        token = strtok(temp, ",");
+        while (token != NULL)
+        {
+            printf("%9s|", token);
+            token = strtok(NULL, ",");
+        }
+        row++;
+    }
+    printf("\nCol: %d, ", col);
+    printf("Row: %d", row);
+
+    
+}
